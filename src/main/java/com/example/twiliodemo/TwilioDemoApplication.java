edbf0527fc1fce4362d86979a1a2e0536225ddec
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-25 15:25:13 -0500

    add apprunner

--- src/main/java/com/example/twiliodemo/TwilioDemoApplication.java
+++ src/main/java/com/example/twiliodemo/TwilioDemoApplication.java
@@ -1,11 +1,13 @@
 package com.example.twiliodemo;
 
 import com.twilio.Twilio;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwilioDemoApplication {
+public class TwilioDemoApplication implements ApplicationRunner {
 
     private final static String ACCOUNT_SID = "<your-account-sid>";
     private final static String AUTH_ID = "<your-auth-id>";
@@ -18,4 +20,8 @@ public class TwilioDemoApplication {
     }
 
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
