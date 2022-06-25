0de19ee753f71d031757fbce4e8a79eb9bfba01e
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-25 15:24:37 -0500

    add init

--- src/main/java/com/example/twiliodemo/TwilioDemoApplication.java
+++ src/main/java/com/example/twiliodemo/TwilioDemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.twiliodemo;
 
+import com.twilio.Twilio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwilioDemoApplication {
 
+    private final static String ACCOUNT_SID = "<your-account-sid>";
+    private final static String AUTH_ID = "<your-auth-id>";
+
+    static {
+        Twilio.init(ACCOUNT_SID, AUTH_ID);
+    }
     public static void main(String[] args) {
         SpringApplication.run(TwilioDemoApplication.class, args);
     }
+
 
 }
